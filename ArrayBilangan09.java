c767c8c5d0e20072565e304ff6245ea0eb2b64f7
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-10-31 04:11:34 +0700

    pertanyaan 2-3

--- ArrayBilangan09.java
+++ ArrayBilangan09.java
@@ -8,11 +8,9 @@ public class ArrayBilangan09 {
         bil[2] = -7;
         bil[3] = 17;
 
-    System.out.println(bil[0]);
-    System.out.println(bil[1]);
-    System.out.println(bil[2]);
-    System.out.println(bil[3]);
-    
+    for (int i = 0; i < 4; i ++)
+    System.out.println(bil[i]);
+
 
 
     }
